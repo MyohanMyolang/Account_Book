80e726cf5a87597882e623cd548af8f7368ee013
Myolang <myohanmyolang@gmail.com>
2023-03-13 19:23:26 +0900

    1

--- lib/widgets/detail_page_item.dart
+++ lib/widgets/detail_page_item.dart
@@ -100,7 +100,43 @@ class _DetailItemState extends State<DetailItem> {
                   ),
                   IconButton(
                     onPressed: () {
-                      // ctrl.delete
+                      showDialog(
+                        context: context,
+                        barrierDismissible: true,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text(
+                              "정말로 삭제 하시겠습니까?",
+                              style: TextStyle(color: Colors.red),
+                            ),
+                            content: SingleChildScrollView(
+                              child: ListBody(
+                                children: const [
+                                  Text('삭제하시기를 원하시면 확인 버튼을 눌러주세요.')
+                                ],
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  widget.onRemove(widget.model);
+                                },
+                                child: const Text(
+                                  "확인",
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text("취소"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
                     },
                     icon: const Icon(
                       Icons.remove_circle,
